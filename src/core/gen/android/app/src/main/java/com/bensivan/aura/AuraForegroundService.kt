9630fd3f9cbc810b8f1b88f7ae1e18e8496f7a6c
BenSiv <bensiv92@gmail.com>
2026-05-18 17:37:05 -0700

    fix(service): resolve Android 14 MissingForegroundServiceTypeException by passing type parameter in startForeground API and bump release to version 0.1.3 (1003)

--- src/core/gen/android/app/src/main/java/com/bensivan/aura/AuraForegroundService.kt
+++ src/core/gen/android/app/src/main/java/com/bensivan/aura/AuraForegroundService.kt
@@ -28,7 +28,15 @@ class AuraForegroundService : Service() {
             .setOngoing(true)
             .build()
 
-        startForeground(NOTIFICATION_ID, notification)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startForeground(
+                NOTIFICATION_ID, 
+                notification, 
+                android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
+            )
+        } else {
+            startForeground(NOTIFICATION_ID, notification)
+        }
         return START_STICKY
     }
 
