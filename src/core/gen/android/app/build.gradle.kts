3f1eeb850a41b9af495ee254ef6f55e4185fdbad
BenSiv <bensiv92@gmail.com>
2026-05-20 11:35:18 -0700

    fix: update Android build.gradle keystore path to match auraradar project name

--- src/core/gen/android/app/build.gradle.kts
+++ src/core/gen/android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     }
     signingConfigs {
         create("release") {
-            storeFile = file("/home/bensiv/Projects/aura/cfg/release.keystore")
+            storeFile = file("/home/bensiv/Projects/auraradar/cfg/release.keystore")
             storePassword = "aurapassword"
             keyAlias = "aura_alias"
             keyPassword = "aurapassword"
